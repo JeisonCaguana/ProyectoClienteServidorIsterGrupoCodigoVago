9fedbfe49d916a65ad34b06e690ffb60b1032842
JeisonCaguana <jeisoncaguana@gmail.com>
2019-02-21 13:37:44 -0500

    PAOLA VILLA - JEISON CAGUANA  - GRUPO CODIGO VAGO

---
+++ src/com/sansana/controlador/ClPrincipal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sansana.controlador;
+
+import com.sansana.vista.FrmLogin;
+
+/**
+ *
+ * @author JEISON CAGUANA - PAOLA VILLA - GRUPO CODIGO VAGO
+ */
+public class ClPrincipal {
+    public static void main(String[] args) {
+        FrmLogin FrmLogin = new FrmLogin();
+        FrmLogin.setVisible(true);
+    }
+}
---
+++ src/com/sansana/modelo/clMedicamentos.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sansana.modelo;
+
+/**
+ *
+ * @author JEISON CAGUANA - PAOLA VILLA - GRUPO CODIGO VAGO
+ */
+public class clMedicamentos {
+    
+}
